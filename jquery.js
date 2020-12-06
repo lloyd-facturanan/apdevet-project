26022edc107a38e59d7e6c90d3d5ad8417e8977b
lloyd-facturanan <75427284+lloyd-facturanan@users.noreply.github.com>
2020-12-05 23:35:23 -0800

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
+	// Show / Hide Main Navigation on Small Devices 
 	$("#menu-btn").click(function(){
 		$("#hidden-nav").slideToggle('slow');
 	});
+
+	// Hide Main Navigation when you click on it
+	$(".test").click(function(){
+		$("#hidden-nav").slideUp(500);
+	})
 });
